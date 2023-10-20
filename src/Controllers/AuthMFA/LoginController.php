1361ac1e39dd4c6cb6719ce3d820193a1c050076
root <root@LT-40CFCS3.localdomain>
2023-10-20 12:18:37 +0530

    new code issues fixed

--- src/Controllers/AuthMFA/LoginController.php
+++ src/Controllers/AuthMFA/LoginController.php
@@ -2,16 +2,75 @@
 
 namespace App\Http\Controllers\AuthMFA;
 
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Sq1\AuthMfa\MFAServices\MFAService;
 
 class LoginController extends Controller
 {
+
+    use MFAService;
+
     /**
+     * Instantiate a new controller instance.
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+     /**
      * show return value
      */
     public function index(Request $request)
     {
+        return app_path();
         return view('auth-mfa.login');
     }
+
+    /**
+     * Handle an authentication attempt.
+     */
+    public function authenticate(Request $request)
+    {
+
+       
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+ 
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return $this->verifyMFA();
+        }
+ 
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+
+     /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        $user = auth()->user();
+        $user->activity = 'offline';
+        $user->save();
+
+        session()->flush();
+        Auth::logout();
+        return redirect()->intended();
+    }
+
+
+
+
 }
--- src/Controllers/AuthMFA/MFAController.php
+++ src/Controllers/AuthMFA/MFAController.php
@@ -2,23 +2,60 @@
 
 namespace App\Http\Controllers\AuthMFA;
 
+use Carbon\Carbon;
+use Cache;
 use Sq1\AuthMfa\MFAServices\MFAService;
+use App\Http\Requests\MFAValidationSecret;
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class MFAController extends Controller
 {
     use MFAService;
-    
+
+
+     /**
+     * Instantiate a new controller instance.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('authGuestMFA');
+    }
+
     /**
      * Display the MFA view.
      *
      * @return \Illuminate\View\View
      */
-    public function verifyMFA()
+    public function indexVerification() : View
     {
-        return '$this->mfaRegister()';
+        return view('auth-mfa.mfa');
         
     }
-    
+
+    /**
+     * Display the MFA view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function mfaVerification(MFAValidationSecret $request)
+    {
+
+        $user = auth()->user();
+        $key = $user->id . ':' . $request->totp;
+
+        if($user->user_status != 'completed') {
+            $user->user_status = 'completed';
+        }
+
+        session()->put('user_current_status', 'verified');
+        //use cache to store token to blacklist
+        Cache::add($key, true, 4);
+        return redirect()->intended();
+  
+    }
+
+
 }
